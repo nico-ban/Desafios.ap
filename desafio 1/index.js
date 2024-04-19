d773fe96482ae87a0a64c6bcc63aee7e84284e43
Nicolás Banti <nicolasbanti@gmail.com>
2024-04-19 20:06:43 -0300

    Prueba

---
+++ desafio 1/index.js
@@ -0,0 +1,22 @@
+let NombreUsuario1 =  prompt("ingresa tu nombre:");
+let Numero1 = 14
+let Numero2 = 5
+
+Numero1 = 16
+
+const Numero3 = 18
+
+console.log("Tu nombre es: " + NombreUsuario1)
+
+let nombre = prompt("Agrega tu nombre: ")
+let edad = prompt("Agrega tu edad: ")
+let anioActual = prompt("Ingrese los cuatro digitos del año actual: ")
+let nacimiento = parseInt(anioActual - edad)
+
+let jubH = nacimiento + 65;
+let jubM = nacimiento + 60;
+
+alert(nombre + "se jubila en : " + jubH)
+
+
+
